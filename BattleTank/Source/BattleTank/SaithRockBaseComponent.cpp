690b3a1b366f64084e7b4e8dfaf27a4f77af1960
Saithraphim <nupaul.miller@gmail.com>
2020-06-15 23:06:21 -0400

    Adding more features

--- BattleTank/Source/BattleTank/SaithRockBaseComponent.cpp
+++ BattleTank/Source/BattleTank/SaithRockBaseComponent.cpp
@@ -6,13 +6,13 @@
 // Sets default values for this component's properties
 USaithRockBaseComponent::USaithRockBaseComponent()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  
+	// You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
 	// ...
 }
-
 
 // Called when the game starts
 void USaithRockBaseComponent::BeginPlay()
@@ -20,15 +20,14 @@ void USaithRockBaseComponent::BeginPlay()
 	Super::BeginPlay();
 
 	// ...
-	
 }
 
-
 // Called every frame
-void USaithRockBaseComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void USaithRockBaseComponent::TickComponent(float DeltaTime, 
+	ELevelTick TickType, 
+	FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 	// ...
 }
-
--- BattleTank/Source/BattleTank/SaithRockBaseComponent.h
+++ BattleTank/Source/BattleTank/SaithRockBaseComponent.h
@@ -22,5 +22,7 @@ protected:
 
 public:	
 	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	virtual void TickComponent(float DeltaTime, 
+		ELevelTick TickType, 
+		FActorComponentTickFunction* ThisTickFunction) override;
 };
