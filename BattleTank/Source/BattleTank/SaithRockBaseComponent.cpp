ef9c377bf77cedce0fc62e3b4b0938373eb6a19c
Saith <nupaul.miller@gmail.com>
2020-06-28 23:54:19 -0400

    Minor changes

--- BattleTank/Source/BattleTank/SaithRockBaseComponent.cpp
+++ BattleTank/Source/BattleTank/SaithRockBaseComponent.cpp
@@ -6,7 +6,8 @@
 // Sets default values for this component's properties
 USaithRockBaseComponent::USaithRockBaseComponent()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  
+	// Set this component to be initialized when the game starts, 
+	// and to be ticked every frame.  
 	// You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
